51f9efb50cafd3454123bceecc2744de88f619d9
MrozKarol <polityk357@gmail.com>
2019-05-12 18:22:16 +0200

    new start

---
+++ index.js
@@ -0,0 +1,5 @@
+var today = new Date();
+var year = today.getFullYear();
+var footer = document.querySelector(".footer");
+
+footer.innerHTML = "<h5> Copyrigth &copy;" + year + " wszelkie prawa zastrzeżone </h5>"
